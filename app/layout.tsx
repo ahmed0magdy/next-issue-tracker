23a0230ad87ec57d687b8dd58c238722c654ebf2
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2024-12-22 10:02:27 +0200

    adding RadixUI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "@radix-ui/themes/styles.css";
 import "./globals.css";
 import { Geist, Geist_Mono } from "next/font/google";
 import NavBar from "./NavBar";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,9 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme>
+        <Theme accentColor="yellow" radius="large">
           <NavBar />
           <main className="p-5">{children}</main>
+          {/* <ThemePanel /> */}
         </Theme>
       </body>
     </html>
