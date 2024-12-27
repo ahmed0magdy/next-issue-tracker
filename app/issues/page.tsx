aeac876e1defcc6b3c07b0859c92770358708450
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2024-12-27 08:51:10 +0200

    refactor import components

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,7 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import IssueActions from "./IssueActions";
-import { IssueStatusBadge, CustomLink } from "@/app/components";
+import { IssueStatusBadge, Link } from "@/app/components";
 
 const issuesPage = async () => {
   const issues = await prisma.issue.findMany();
@@ -24,9 +24,7 @@ const issuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <CustomLink href={`/issues/${issue.id}`}>
-                  {issue.title}
-                </CustomLink>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status}></IssueStatusBadge>
                 </div>
