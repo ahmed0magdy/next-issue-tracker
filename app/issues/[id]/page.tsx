79aacd57bb0fa5de2dd7a48837ebcd4f7c925f49
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2025-01-08 13:58:19 +0200

    Add metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -8,11 +8,11 @@ import { getServerSession } from "next-auth";
 import authOptions from "@/app/api/auth/authOptions";
 import AssigneeSelect from "./AssigneeSelect";
 
-const issueDetailPage = async ({
-  params,
-}: {
+interface Props {
   params: Promise<{ id: string }>;
-}) => {
+}
+
+const issueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
   const { id } = await params;
   const issue = await prisma.issue.findUnique({
@@ -32,4 +32,15 @@ const issueDetailPage = async ({
     </Grid>
   );
 };
+export async function generateMetadata({ params }: Props) {
+  const { id } = await params;
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  return {
+    title: issue?.title,
+    description: "Details of issue " + issue?.id,
+  };
+}
 export default issueDetailPage;
